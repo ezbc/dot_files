54ac11762e385b24d1c14986df630e223284dfd4
Elijah Bernstein-Cooper <e.bernsteincooper@gmail.com>
2015-11-07 08:48:20 -0600

    ?

--- write_symlinks.sh
+++ write_symlinks.sh
@@ -3,5 +3,6 @@ export DOTLOC=/home/ezbc/dot_files
 ln -s $DOTLOC/.new_vim ~/.new_vim
 ln -s $DOTLOC/.vimrc ~/.vimrc
 ln -s $DOTLOC/.bashrc ~/.bashrc
+ln -s $DOTLOC/.bash_profile ~/.bash_profile
 ln -s $DOTLOC/.tcshrc ~/.tcshrc
 
