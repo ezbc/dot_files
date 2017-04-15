346b5a79fda36829f92b8f4a166f0ab7301f1172
Elijah Bernstein-Cooper <e.bernsteincooper@gmail.com>
2017-04-15 17:07:36 -0500

    setup script for environment

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# make the opt directory
+mkdir -p ~/opt
+
+# get a script for git auto completion
+wget -O ~/opt/git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash 
+
